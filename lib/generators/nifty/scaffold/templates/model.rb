c78b6b1c9cea3f241acb91d76690bb97bd239c84
Brandon Martin <bmartin@zyphmartin.com>
2010-06-21 12:36:13 -0700

    make changes for mongoid

--- lib/generators/nifty/scaffold/templates/model.rb
+++ lib/generators/nifty/scaffold/templates/model.rb
@@ -9,10 +9,12 @@ class <%= class_name %><%= " < #{options[:parent].classify}" if options[:parent]
   include Mongoid::Versioning
 <% end -%>
   
-<% model_attributes.reject{|attr| attr.reference?}.each do |attribute| -%>
+<% model_attributes.reject{|attr| attr.reference? }.each do |attribute| -%>
   field :<%= attribute.name %>, :type => <%= attribute.type_class %>
 <% end -%>
-<% model_attributes.select{|attr| attr.reference? }.each do |attribute| -%>
-  embedded_in :<%= attribute.name%>, :inverse_of => :<%= class_name.tableize %>
+
+<% if options[:embedded] -%>
+  embedded_in :<%= #{options[:embedded]} %>, :inverse_of => :<%= class_name.tableize %>
 <% end -%>
+
 end
